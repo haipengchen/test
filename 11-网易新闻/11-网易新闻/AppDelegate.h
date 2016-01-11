22006eec3d66a44a88714ae24df1bee20dbd100b
陈海鹏 <chenhaipeng@chenshutingdemacbook-air.local>
2016-01-11 23:38:10 +0800

    水水水水

---
+++ 11-网易新闻/11-网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  11-网易新闻
+//
+//  Created by 陈海鹏 on 16/1/11.
+//  Copyright © 2016年 陈海鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 11-网易新闻/11-网易新闻/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  11-网易新闻
+//
+//  Created by 陈海鹏 on 16/1/11.
+//  Copyright © 2016年 陈海鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
